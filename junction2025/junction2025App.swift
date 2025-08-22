a890b3ca5b929bcb80c2bd1cc499f3df51f3d5c0
heracloud <heracloud7@naver.com>
2025-08-23 01:47:11 +0900

    Initial Commit

---
+++ junction2025/junction2025App.swift
@@ -0,0 +1,17 @@
+//
+//  junction2025App.swift
+//  junction2025
+//
+//  Created by 임유리 on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct junction2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
