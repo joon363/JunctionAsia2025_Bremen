e5b34907b92b7a266cc5462719017ba76373753f
joon363 <joon363@naver.com>
2025-08-23 23:24:55 +0900

    Refactoring

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'quiz_widget.dart';
 import 'post_widget.dart';
 import 'theme.dart';
 
@@ -13,12 +15,14 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late Future<List<dynamic>> _postsFuture;
+  late Future<List<dynamic>> _wordsFuture;
 
   @override
   void initState() {
     super.initState();
     // Load post data from the JSON asset when the widget is first created
     _postsFuture = _loadPostsData();
+    _wordsFuture = _loadUnknownWords();
   }
 
   // Asynchronously loads and decodes post data from a JSON file in the assets.
@@ -31,11 +35,24 @@ class _HomeScreenState extends State<HomeScreen> {
     return jsonList;
   }
 
+  Future<List<dynamic>> _loadUnknownWords() async {
+    // Load the JSON string from the asset bundle
+    final String jsonString =
+    await rootBundle.loadString('assets/voca_user.json');
+    // Decode the JSON string into a List
+    final List<dynamic> jsonList = json.decode(jsonString);
+    if (jsonList.length <= 3) {
+      // 요소가 3개 이하라면 그대로 반환
+      return jsonList;
+    }
+    jsonList.shuffle(Random()); // 리스트 순서를 랜덤으로 섞음
+    return jsonList.take(3).toList(); // 앞에서 3개 추출
+  }
   @override
   Widget build(BuildContext context) {
     // CHANGE: Removed the Scaffold and AppBar as they are now in MainScreen
-    return FutureBuilder<List<dynamic>>(
-      future: _postsFuture,
+    return FutureBuilder(
+      future: Future.wait([_postsFuture,_wordsFuture]),
       builder: (context, snapshot) {
         // Show a loading indicator while data is being fetched
         if (snapshot.connectionState == ConnectionState.waiting) {
@@ -47,7 +64,9 @@ class _HomeScreenState extends State<HomeScreen> {
         }
         // Once data is successfully loaded, build the list of posts
         else if (snapshot.hasData) {
-          final postData = snapshot.data!;
+          final results = snapshot.data as List<dynamic>;
+          final postData = results[0] as List<dynamic>;
+          final wordList = results[1] as List<dynamic>;
           return PageView.builder(
             scrollDirection: Axis.vertical, // Set scroll direction to vertical.
             //physics: const FastScrollPhysics(),
@@ -56,11 +75,15 @@ class _HomeScreenState extends State<HomeScreen> {
               // Ensure the post is a Map before passing it to the widget
               final post = postData[index] as Map<String, dynamic>;
               var pair = RandomColor();
-              return PostWidget(
-                post: post,
-                backgroundColor: pair.backgroundColor,
-                textColor: pair.textColor
-              );
+              if(index==0||index%5!=0) {
+                return PostWidget(
+                    post: post,
+                    backgroundColor: pair.backgroundColor,
+                    textColor: pair.textColor
+                );
+              } else{
+                return QuizWidget(words: wordList);
+              }
             },
           );
         }
