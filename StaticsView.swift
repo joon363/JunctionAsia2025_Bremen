69d8e20a8f7a61d8f87faae6c7c5bf40e53ae47d
heracloud <heracloud7@naver.com>
2025-08-23 12:09:58 +0900

    통계 파트 보완

--- StaticsView.swift
+++ StaticsView.swift
@@ -23,10 +23,38 @@ struct StaticsView: View {
                     .aspectRatio(contentMode: .fit)
                 Text("  ")
                 Text("  ")
-                
+                HStack {
+                    Text("학습 단어 수")
+                        .font(.system(size: 24))
+                    Spacer()
+                    Text("187개")
+                        .bold()
+                        .foregroundStyle(Color("Orange"))
+                        .font(.system(size: 32))
+                }
+
+                Text("(상위 3.7%)")
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+
+                HStack{
+                    Text("읽은 포스트 수")
+                        .font(.system(size: 24))
+                    Spacer()
+                    Text("213개")
+                        .bold()
+                        .foregroundStyle(Color("Orange"))
+                        .font(.system(size: 32))
+
+
+                }.font(.system(size: 24))
+
+                Text("(상위 5.3%)")
+                    .frame(maxWidth: .infinity, alignment: .trailing)
                 Image("Grace")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
+                Text("  ")
+                Text("토익 RC Score 420점의 수준이예요")
             }
         }
     }
